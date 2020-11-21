ffdef18930644626b2d5f33a804bd9f74f2ee916
THANGA MANICKAM M <thanga.manickam5@gmail.com>
2020-11-22 03:18:15 +0530

    profilepageintegration

---
+++ src/main/webapp/WEB-INF/classes/Model/DataConnector.java
@@ -0,0 +1,48 @@
+package Model;
+import java.io.*;
+import java.sql.*;
+public class DataConnector {
+	public static Connection connect()
+	{
+		Connection connection=null;
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver"); 
+			connection = DriverManager.getConnection("jdbc:mysql://ncp-project.c9pmlhgdvlrz.us-east-2.rds.amazonaws.com/AlumniPortal",
+							"alumni",
+							"fromarea51"
+							);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Error!");
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+	public static UserBase getRecordById(String rollNumber){  
+	    UserBase u=null;  
+	    try{  
+	        Connection con= connect();  
+	        PreparedStatement ps=con.prepareStatement("select * from profile_home_academic where "
+	        		+ "RollNumber=?");  
+	        ps.setString(1, rollNumber);  
+	        ResultSet rs=ps.executeQuery();  
+	        while(rs.next()){  
+	            u=new UserBase();
+	            System.out.println(rs.getString("RollNumber"));
+	            u.setRollNumber(rs.getString("RollNumber"));  
+	            u.setFirstName(rs.getString("FirstName"));  
+	            u.setLastName(rs.getString("LastName"));  
+	            u.setOfficialEmail(rs.getString("OfficialEmail"));  
+	            u.setPersonalEmail(rs.getString("PersonalEmail"));  
+	            u.setMobileNumber(rs.getString("MobileNumber"));
+	            u.setCity(rs.getString("City"));
+	            u.setShortBio(rs.getString("ShortBio"));
+	        }  
+	    }catch(Exception e){System.out.println(e);}  
+	    return u;  
+	}  
+	
+}
---
+++ src/main/webapp/WEB-INF/classes/Model/UserBase.java
@@ -0,0 +1,93 @@
+package Model;
+public class UserBase {
+
+	String rollNumber,firstName,lastName,mobileNumber,officialEmail,personalEmail,
+	city,shortBio,courseEnrolled,academicBatch,dept,academicAdvisor;
+	float cgpa;
+	UserBase()
+	{
+		rollNumber = "";firstName="";lastName="";mobileNumber="";officialEmail="";
+		personalEmail="";city="";shortBio="";courseEnrolled="";academicBatch="";dept="";
+		academicAdvisor="";
+		cgpa=-1;
+	}
+	//generate Getters andSetters
+	public String getRollNumber() {
+		return rollNumber;
+	}
+	public void setRollNumber(String rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getOfficialEmail() {
+		return officialEmail;
+	}
+	public void setOfficialEmail(String officialEmail) {
+		this.officialEmail = officialEmail;
+	}
+	public String getPersonalEmail() {
+		return personalEmail;
+	}
+	public void setPersonalEmail(String personalEmail) {
+		this.personalEmail = personalEmail;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getShortBio() {
+		return shortBio;
+	}
+	public void setShortBio(String shortBio) {
+		this.shortBio = shortBio;
+	}
+	public String getCourseEnrolled() {
+		return courseEnrolled;
+	}
+	public void setCourseEnrolled(String courseEnrolled) {
+		this.courseEnrolled = courseEnrolled;
+	}
+	public String getAcademicBatch() {
+		return academicBatch;
+	}
+	public void setAcademicBatch(String academicBatch) {
+		this.academicBatch = academicBatch;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public String getAcademicAdvisor() {
+		return academicAdvisor;
+	}
+	public void setAcademicAdvisor(String academicAdvisor) {
+		this.academicAdvisor = academicAdvisor;
+	}
+	public float getCgpa() {
+		return cgpa;
+	}
+	public void setCgpa(float cgpa) {
+		this.cgpa = cgpa;
+	}
+}
