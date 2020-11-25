6f6314cc5d94edab86270f4edd30f9e137f04782
THANGA MANICKAM M <thanga.manickam5@gmail.com>
2020-11-26 02:18:32 +0530

    Thanga : Big Bombs dropping

--- compilejava.bat
+++ compilejava.bat
@@ -1,7 +1,9 @@
-cd src\main\webapp\WEB-INF\classes\Model
 del /S *.class
+cd src\main\webapp\WEB-INF\classes\Model
 javac JobsBase.java
 javac AchievementsBase.java
 javac -cp ..\ UserBase.java
 javac -cp ..\ DataConnector.java
 javac -cp ..\ RandomIdGenerator.java
+cd ..\ServletPackage
+javac -cp ..\ PerformLogin.java
