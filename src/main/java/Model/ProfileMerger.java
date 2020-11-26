e34a2b79f14d54b70e2a78e136204d0c56a547b0
THANGA MANICKAM M <thanga.manickam5@gmail.com>
2020-11-26 15:41:49 +0530

    fixing javabeans merge

--- src/main/java/Model/ProfileMerger.java
+++ src/main/java/Model/ProfileMerger.java
@@ -5,12 +5,12 @@ public class ProfileMerger extends BeanUtilsBean{
     @Override
     public void copyProperty(Object dest, String name, Object value)
             throws IllegalAccessException, InvocationTargetException {
-        if(value.getClass().getName("java.lang.String"))
+        if(value.getClass().getName().equals("java.lang.String"))
         {
             if((String)value=="")return;
             super.copyProperty(dest, name, value);
         }
-        else if(value.getClass().getName("java.lang.Float"))
+        else if(value.getClass().getName().equals("java.lang.Float"))
         {
             if(-1==((Float)value))return;
             super.copyProperty(dest, name, value);
