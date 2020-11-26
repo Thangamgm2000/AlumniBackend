02fc0f982497f776043829c6e581f0d03d72ff8f
THANGA MANICKAM M <thanga.manickam5@gmail.com>
2020-11-26 15:36:29 +0530

    Fixing javabeancopy

--- src/main/java/Model/ProfileMerger.java
+++ src/main/java/Model/ProfileMerger.java
@@ -5,8 +5,24 @@ public class ProfileMerger extends BeanUtilsBean{
     @Override
     public void copyProperty(Object dest, String name, Object value)
             throws IllegalAccessException, InvocationTargetException {
+    try{
+        if(value.getClass().getName("java.lang.String"))
+        {
+            if((String)value=="")return;
+            super.copyProperty(dest, name, value);
+        }
+        else if(value.getClass().getName("java.lang.Float"))
+        {
+            if(-1==((Float)value))return;
+            super.copyProperty(dest, name, value);
+        }
         if((String)value=="")return;
-        if(new Float(-1).equals(value))return;
-        super.copyProperty(dest, name, value);
+        
+    }
+    catch (Exception e)
+    {
+        if(-1==((Float)value))return;
+    }
+        
     }
 }
